9e00b3ee766faee5ec747c4edfe36557e341c466
tashenoff <kail.dead.kd@gmail.com>
2026-05-05 12:30:16 +0600

    .

--- src/components/FloatingPhoneButton.js
+++ src/components/FloatingPhoneButton.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 function FloatingPhoneButton() {
-    const phoneNumber = "877005273070"; // Номер телефона для набора
+    const phoneNumber = "87005273070"; // Номер телефона для набора
 
     const handleClick = () => {
         window.location.href = `tel:${phoneNumber}`; // Переход к набору номера
